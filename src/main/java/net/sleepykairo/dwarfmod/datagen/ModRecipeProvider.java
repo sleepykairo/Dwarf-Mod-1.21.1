2954ece8f38edee580cf39b3776e433cf480489e
sleepykairo <goodnightkairo@gmail.com>
2024-10-01 10:49:37 -0500

    "v1.0.0" pretty much just mithril stuff but hey it works

--- src/main/java/net/sleepykairo/dwarfmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/sleepykairo/dwarfmod/datagen/ModRecipeProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
@@ -24,7 +25,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     public void generate(RecipeExporter exporter) {
         List<ItemConvertible> MITHRIL_SMELTABLES = List.of(ModItems.RAW_MITHRIL, ModBlocks.MITHRIL_ORE, ModBlocks.DEEPSLATE_MITHRIL_ORE);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.MITHRIL_MINECART)
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TRANSPORTATION, ModItems.MITHRIL_MINECART)
                 .pattern("OOO")
                 .pattern("MCM")
                 .pattern("OOO")
@@ -34,6 +35,81 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
                 .offerTo(exporter);
 
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.MITHRIL_SWORD)
+                .pattern(" M ")
+                .pattern(" M ")
+                .pattern(" S ")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.MITHRIL_SHOVEL)
+                .pattern(" M ")
+                .pattern(" S ")
+                .pattern(" S ")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.MITHRIL_PICKAXE)
+                .pattern("MMM")
+                .pattern(" S ")
+                .pattern(" S ")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.MITHRIL_AXE)
+                .pattern("MM ")
+                .pattern("MS ")
+                .pattern(" S ")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.MITHRIL_HOE)
+                .pattern("MM ")
+                .pattern(" S ")
+                .pattern(" S ")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .input('S', Items.STICK)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.MITHRIL_HELMET)
+                .pattern("MMM")
+                .pattern("M M")
+                .pattern("   ")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.MITHRIL_CHESTPLATE)
+                .pattern("M M")
+                .pattern("MMM")
+                .pattern("MMM")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.MITHRIL_LEGGINGS)
+                .pattern("MMM")
+                .pattern("M M")
+                .pattern("M M")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.MITHRIL_BOOTS)
+                .pattern("   ")
+                .pattern("M M")
+                .pattern("M M")
+                .input('M', ModItems.MITHRIL_INGOT)
+                .criterion(hasItem(ModItems.MITHRIL_INGOT), conditionsFromItem(ModItems.MITHRIL_INGOT))
+                .offerTo(exporter);
+
+        //blocks
+
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, ModItems.MITHRIL_INGOT, RecipeCategory.BUILDING_BLOCKS, ModBlocks.MITHRIL_BLOCK);
+
+        //smelting/other
 
         offerSmelting(exporter, MITHRIL_SMELTABLES, RecipeCategory.MISC, ModItems.MITHRIL_INGOT, 1.5f, 400, "mithril_ingot");
         offerBlasting(exporter, MITHRIL_SMELTABLES, RecipeCategory.MISC, ModItems.MITHRIL_INGOT, 1.5f, 200, "mithril_ingot");
